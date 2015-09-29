47b94cc738818d0a4889322e999463b49689ab1a
jonataseduardo <jonataseduardo@gmail.com>
2015-09-30 00:07:05 +0400

    seting erdos paramns

--- StructRandom/run_darwin.py
+++ StructRandom/run_darwin.py
@@ -7,35 +7,39 @@ from subprocess import Popen, PIPE, STDOUT
 def get_params():
     
     num        = "400"
-    graph      = "1" # Watts Strogats 1, Barabasi 2
+    graph      = ["1", "2", "3"] # 1 - Watts Strogats, 2 - Barabasi, 3 - Erdos Renyi
     tstep      = "1.0"
-    coleta     = "1000"
-    burn       = "500"
+    coleta     = "10000"
+    burn       = "800"
     tth        = "0.65"
-    nmasters   = "2"
-    nsamples   = "2"
+    nmasters   = "20"
+    nsamples   = "50"
     
-    delta = np.linspace(0,1,3) 
+    delta = np.linspace(0,1,11) 
 
-    beta = np.linspace(0,8,3) 
+    beta = np.linspace(0,10,11) 
 
-    # viz = range(4,20,2)
-    recon_prob = np.linspace(0,1,5)
+    def recon_prob(g):
+        if g == "3": #from 4 to 20 neighbours on Erdos Renyi
+            return  np.linspace(0.01,0.05,21) # if p > 1 the num of neigbs is 4
+        else:
+            return np.linspace(0,1,21) 
 
     params = [ '%f'%d   + " " +
                '%f'%b   + " " +
                "%f"%p   + " " +
                num      + " " +
-               graph    + " " +
+               g        + " " +
                tstep    + " " +
                coleta   + " " +
                burn     + " " +
                tth      + " " +
                nmasters + " " +
                nsamples   
-               for p in recon_prob
+               for g in graph
                for d in delta
-               for b in reversed(beta) ]
+               for b in reversed(beta)
+               for p in recon_prob(g)]
         
     return params
 
@@ -48,29 +52,31 @@ def pp(cmd):
 
 
 def main(params):
+    print params
     return [params, pp('./roda ' + params )]
 
 
-def organize(rdata, params):
+def organize(rdata):
     """
     Params
     ------
     rdata: [params, (string(hist + result), None)]
     """
-     
-    rdata = pd.Series([i[0] + i[1][0].split('\n')[-1] for i in out])
-    rdata
-    rdata[0]
-    rdata[0][1][0].split('\n')[-1]
+
+    names = ["delta", "beta", "p", "num", "graph", "tstep", "coleta", "burn", "tth",
+             "nmasters", "nsamples", "etau", "eetau", "mtau", "emtau", "mag", "vmag", 
+             "sigma", "erg", "verg", "acpt", "vacpt", "n", "TotalTime", "hist"]
+
+    out = [(i[0] + i[1][0].split('\n')[-1]).split() + [':'.join(i[1][0].split('\n')[:-1])] for i in rdata]
+    return pd.DataFrame(out, columns=names)
+
 
 
 if __name__ == '__main__':
 
     params = get_params()
-    params[0]
-    pp('echo {} > input'.format(params[0]))
-    
-    pool = Pool(processes=20)
+    pool = Pool(processes=120)
     resp = pool.map_async(main, params)
     out = [i for i in  resp.get()]
-
+    data = organize(out)
+    data.to_csv('teste_5.csv')
